8bf1c23ea10c561050448db7eb83ae44fcc27df9
UMA <trueSuperiorDev@gmail.com>
2018-11-24 00:24:11 +0900

    Add devServer option
    - ブラウザリロードのためのオプションを追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     // ローカル開発用環境を立ち上げる
     // 実行時にブラウザが自動的に localhost を開く
     devServer: {
+        inline: true,
         watchContentBase: true,
         contentBase: 'src',
         open: true
