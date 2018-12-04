33bc951e74d1146a93fda50bb3d292b6ae8901b4
y-ikeda <y-ikeda@tenik.co.jp>
2018-12-04 10:23:27 +0900

    Update webpack.config.js
    - cssのソースマップを追加する設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
+const dev = 'development';
+const pro = 'production';
+
+const MODE = dev;
+const enabledSourceMap = (Mode === dev);
+
 module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
-    mode: 'development',
+    mode: MODE,
     module: {
         rules: [
             {
                 test: /\.css/,
                 use: [
                     'style-loader',
-                    { loader: 'css-loader', options: { url: false } },
+                    {
+                        loader: 'css-loader',
+                        options: { 
+                            url: false,
+                            minimize: true, 
+                            sourceMap: enabledSourceMap
+                        }
+                    },
                 ],
             },
         ]
