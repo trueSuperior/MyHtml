464e81032eaf893ae073cc2d4f585d4daa05fd6a
y-ikeda <y-ikeda@tenik.co.jp>
2018-12-05 18:47:51 +0900

    Remove dependencies code

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,4 +1,3 @@
-import {mxClient, mxGraph, mxRubberband, mxUtils, mxEvent, mxConstants, mxEdgeStyle} from 'mxgraph/javascript/mxClient.min.js'
 
 // ロード時
 export function onLoad() {
@@ -8,39 +7,8 @@ export function onLoad() {
 }
 
 function load() {
-    // console.log(mxGraph);
-    if (!mxClient.isBrowserSupported()) {
-        mxUtils.error('Browser is not supported!', 200, false);
-        return;
-    }
-
-    initGraph(document.getElementById('graphContainer'));
-
+    hello();
     showGlobalVariables();
-}
-/**
- * 引数の要素にグラフを描画する
- * @param  {} container グラフを描画するDOM要素
- */
-function initGraph(container) {
-    var graph = new mxGraph(container);
-    var model = graph.getModel();
-    var parent = graph.getDefaultParent();
-
-    var vertexStyle = graph.getStylesheet().getDefaultVertexStyle();
-    vertexStyle[mxConstants.ROUNDED] = true;
-
-    var edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
-    edgeStyle[mxConstants.STYLE_EDGE] = mxEdgeStyle.TopToBottom;
-
-    model.beginUpdate();
-    try {
-        var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30, 'rounded;strokeColor=red;fillColor=white');
-        var v2 = graph.insertVertex(parent, null, 'World', 200, 150, 80, 30);
-        var e1 = graph.insertEdge(parent, null, '', v1, v2);
-    } finally {
-        model.endUpdate();
-    }
 }
 
 /**
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,10 @@
 const dev = 'development';
 const pro = 'production';
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 // const webpack = require('webpack');
 
 const MODE = dev;
 const enabledSourceMap = (MODE === dev);
-
-/*
-    # mxGraph用の追加設定
-    ES6モジュール化に対応していないのでビルド時にExport/Importの記述を追加して、
-    別モジュールから呼び出せるようにしている。
-
-    モジュール化に依存したライブラリ以下３つ
-    - imports-loader
-    - exports-loader
-    - copy-webpack-plugin
-*/
-const mxGraphConf = {
-    rule: {
-        test: require.resolve('mxgraph/javascript/mxClient.min.js'),
-        use: [
-            'imports-loader?mxBasePath=>"/libs/mxGraph/src/"',
-            'imports-loader?mxLoadResources=>false',
-            'exports-loader?mxClient,mxGraph,mxRubberband,mxUtils,mxEvent,mxConstants,mxEdgeStyle'
-        ]
-    },
-    plugin: {
-        // リソースを公開ディレクトリに配置する
-        from: path.resolve(__dirname, './node_modules/mxgraph/javascript/src'),
-        to: path.resolve(__dirname, 'public/libs/mxGraph/src'),
-        ignore: ['.*','js']
-    }
-};
 
 module.exports = {
     mode: MODE,
@@ -57,15 +29,8 @@ module.exports = {
                     },
                 ],
             },
-            mxGraphConf.rule
         ]
     },
-
-    plugins: [
-        new CopyWebpackPlugin([
-            mxGraphConf.plugin
-        ])
-    ],
 
     // ローカル開発用環境を立ち上げる
     devServer: {
